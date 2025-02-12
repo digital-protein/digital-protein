1554b1533ca9796a0b7be866b30da4af88bb59db
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-12 19:15:17 +0530

    update homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,10 +3,11 @@
 import React from 'react';
 import Head from 'next/head';
 import Navbar from '@/components/common/Navbar';
+import Image from 'next/image';
 
 const HomePage = () => {
     return (
-        <div className='bg-black min-h-screen'>
+        <div className='min-h-screen bg-black'>
 
             <Head>
                 <title>Digital Protein | 2024</title>
@@ -16,13 +17,27 @@ const HomePage = () => {
 
             <Navbar />
 
-            <div className='w-full h-auto flex flex-col gap-6 items-center justify-center'>
+            <div className='w-full h-auto flex flex-col gap-6 items-center justify-center mt-6'>
 
-                <div className='md:w-10/12 w-11/12 h-auto flex md:flex-row flex-col md:gap-0 gap-8 items-center md:pt-16 pt-8 pb-10 bg-black'>
+                <div className='md:w-10/12 w-11/12 h-auto flex md:flex-row flex-col items-center justify-center gap-8'>
+                    <div className='md:w-1/2 w-full h-auto flex flex-col items-start justify-center md:gap-10 gap-6'>
+                        <h1 className='md:text-4xl text-xl font-bold text-white'>Revolutionizing Digital Solutions</h1>
+                        <p className='text-lg text-[#818181] text-justify'>
+                            At Digital Protein, we blend innovation and technology to craft powerful digital experiences.
+                            From cutting-edge web solutions to scalable cloud architectures, we help businesses thrive in
+                            the digital era. Join us in shaping the future of technology.
+                        </p>
+                        <button
+                            className='px-4 py-2 transition-all bg-transparent hover:bg-[#fff] text-[#fff] hover:text-black text-semibold border-2 border-[#fff] rounded-full'
+                        >
+                            Explore More
+                        </button>
+                    </div>
+                    <div className='w-1/2 h-auto flex items-center justify-center'>
+                        <Image src={'/bg/sphere.svg'} alt='sphere' width={550} height={550} priority={true} />
+                    </div>
                 </div>
-
-                <div className='md:w-10/12 w-11/12 h-auto flex md:flex-row flex-col md:gap-0 gap-8 items-center py-20 mb-10 bg-black'>
-                </div>
+                
             </div>
 
         </div>
