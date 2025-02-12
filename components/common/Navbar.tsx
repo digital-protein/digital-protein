bb13045817776c120308408c94d696746a9e7a82
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-12 19:15:07 +0530

    minor fix

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -14,11 +14,11 @@ const Navbar = () => {
     };
 
     return (
-        <div className='w-full h-auto py-6 flex items-center justify-center bg-[#101010]'>
+        <div className='w-full h-auto py-4 flex items-center justify-center bg-[#101010]'>
             <div className='md:w-10/12 w-11/12 h-auto flex flex-row justify-between items-center relative'>
 
                 <Link href='/' className='cursor-pointer'>
-                    <Image src='/logo/digital-protein.svg' alt='logo tedxsliet' width={200} height={80} className='md:w-40 w-20' />
+                    <Image src='/logo/digital-protein.svg' alt='logo tedxsliet' width={200} height={80} className='md:w-28 w-20' />
                 </Link>
 
                 <div className='lg:flex hidden flex-row gap-12 text-base font-normal uppercase transition-all'>
