04f6f798d7b87644a37097514dc60f09a979815a
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-14 00:18:46 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,15 +2,21 @@
 
 import React from 'react';
 import Link from 'next/link';
+import Loader from '@/components/common/Loader';
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Link href="/">
-        <p>Go back to the homepage</p>
-      </Link>
+    <div className='w-full h-screen bg-[#212121] flex flex-col gap-10 items-center justify-center'>
+      <div className='flex flex-col items-center justify-center'>
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <div>
+          Go back to the <Link href="/" className='transition-all font-bold hover:underline'>
+            homepage
+          </Link>
+        </div>
+      </div>
+      <div className="loader"></div>
     </div>
   );
 }
