29d27e09d0387d18c423a58efeb9e514f997e3b7
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-03-13 23:18:58 +0530

    update navigations

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link';
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
+import { Button } from '../ui/button';
 
 const Navbar = () => {
     const router = useRouter();
@@ -15,25 +16,32 @@ const Navbar = () => {
     };
 
     return (
-        <div className='w-full h-auto py-4 flex items-center justify-center z-50 transition-all duration-300 border-b-[1.5px] border-[#E45D25] bg-[#00000080] backdrop-blur-[10px]'>
-            <div className='md:w-10/12 w-11/12 h-auto flex flex-row justify-between items-center relative'>
+        <header className="fixed top-0 left-0 w-full py-4 flex justify-center items-center z-50 transition-all duration-300 border-b-[1.5px] border-[#E45D25] bg-[#00000080] backdrop-blur-[10px]">
 
-                <Link href='/' className='cursor-pointer'>
-                    <Image src='/logo/digital-protein.svg' alt='logo digital protein' width={200} height={80} className='md:w-24 w-20' />
+            <div className='flex flex-row justify-between items-center w-10/12 h-full'>
+                <Link href={'/'}>
+                    <Image src="/logo/digital-protein.svg" alt="digital protein logo" width={200} height={50} className="w-20 h-auto cursor-pointer" />
                 </Link>
 
-                <div className='lg:flex hidden flex-row gap-12 text-base font-normal uppercase transition-all'>
-                    <span className={`cursor-pointer`} onClick={() => router.push('/')}>Home</span>
-                    <span className={`cursor-pointer`} onClick={() => router.push('/services')}>Services</span>
-                    <span className={`cursor-pointer`} onClick={() => router.push('/company')}>Company</span>
-                    <span className={`cursor-pointer`} onClick={() => router.push('/our-team')}>Our Team</span>
-                    <span className={`cursor-pointer`} onClick={() => router.push('/contact')}>Contact Us</span>
-                </div>
-
                 <div className="lg:hidden flex items-center text-white">
-                    <HamburgerMenuIcon onClick={toggleNavbar} className={isActive ? 'hidden' : 'flex w-5 h-5'} />
+                    <HamburgerMenuIcon onClick={() => setIsActive(!isActive)} className={isActive ? 'hidden' : 'flex w-5 h-5'} />
+                    <Cross1Icon onClick={() => setIsActive(!isActive)} className={isActive ? 'flex w-5 h-5' : 'hidden'} />
                 </div>
 
+                <nav className="text-white text-center lg:flex hidden lg:w-auto lg:space-x-8 justify-between items-center w-[55%]">
+                    <Link href={'/services'} className="cursor-pointer hover:text-[#E45D25]">Services</Link>
+                    <Link href={'/industries'} className="cursor-pointer hover:text-[#E45D25]">Industries</Link>
+                    <Link href={'/portfolio'} className="cursor-pointer hover:text-[#E45D25]">Portfolio</Link>
+                    <Link href={'/company'} className="cursor-pointer hover:text-[#E45D25]">Company</Link>
+                    <Link href={'/insights'} className="cursor-pointer hover:text-[#E45D25]">Insights</Link>
+                    <Link href={'/career'} className="cursor-pointer hover:text-[#E45D25]">Career</Link>
+                </nav>
+
+                <Link href='/contact' className="hidden lg:block">
+                    <Button variant={'default'} className="relative text-base font-normal">
+                        Schedule a Call
+                    </Button>
+                </Link>
             </div>
 
             <div className={`${isActive ? 'flex' : 'hidden'} fixed top-0 left-0 w-full h-[100vh] bg-black text-white transition-all flex-col items-center gap-4`}>
@@ -84,7 +92,7 @@ const Navbar = () => {
                 </div>
 
             </div>
-        </div>
+        </header >
     )
 }
 
