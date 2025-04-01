de67ec33dd42dc2f4702db7172cc2b1840cebded
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-04-01 22:42:02 +0530

    update navigation

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -6,79 +6,77 @@ const Footer = () => {
   return (
     <div className='w-full flex flex-col items-center justify-center bg-[#171717] border-t border-t-[#2b2b2b]'>
 
-      <div className="md:w-10/12 w-11/12 h-auto text-[#fff] pt-16 pb-8">
+      <div className="md:w-10/12 w-11/12 h-auto flex flex-col py-4 text-[#fff] md:pt-16 pt-8 pb-8">
 
-        <div className='flex flex-col py-4'>
+        <div className='flex lg:flex-row flex-col items-start justify-between flex-wrap md:py-0 py-6 md:gap-4 gap-8 mb-8'>
 
-          <div className='flex lg:flex-row flex-col items-start justify-between flex-wrap md:py-0 py-6 md:gap-4 gap-8 mb-8'>
-
-            <div className='min-w-64 flex flex-col gap-2 items-start justify-center w-auto text-[14px] text-[#77838F]'>
-              <p className='mb-2 text-[#fff] text-base font-semibold'>Services We Offer</p>
-              <Link href='/' className='font-normal md:w-64 w-full'>Mobile App Development</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>UI & UX Designs</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Software Development</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Management Systems</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>DevOps & Automation</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Blockchain Development</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Cloud Services</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Digital Transformation</Link>
-            </div>
-
-            <div className='min-w-64 flex flex-col gap-2 items-start justify-center w-auto text-[14px] text-[#77838F]'>
-              <p className='mb-2 text-[#fff] text-base font-semibold'>Portfolio</p>
-              <Link href='/' className='font-normal md:w-64 w-full'>Alumni Management</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Institute Webpage</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>E-Commerce Webpage</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Learning Management System</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Portfolio Websites</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Job Portal</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>Event Management System</Link>
-              <Link href='/' className='font-normal md:w-64 w-full'>...more</Link>
-            </div>
-
-            <div className='flex flex-col gap-2 items-start justify-center w-auto text-[14px] text-[#77838F]'>
-              <p className='mb-2 text-[#fff] text-base font-semibold'>Resources</p>
-              <Link href='/company' className='font-normal md:w-64 w-full'>About Company</Link>
-              <Link href='/career' className='font-normal md:w-64 w-full'>Career With Us</Link>
-              <Link href='/contact' className='font-normal md:w-64 w-full'>Contact Us</Link>
-              <Link href='/insights' className='font-normal md:w-64 w-full'>Insights</Link>
-              <Link href='/faqs' className='font-normal md:w-64 w-full'>FAQS</Link>
-              <Link href='/terms-of-service' className='font-normal md:w-64 w-full'>Terms of Service</Link>
-              <Link href='/privacy-policy' className='font-normal md:w-64 w-full'>Privacy Policy</Link>
-            </div>
-
-            <div className='w-auto flex flex-col gap-3'>
-              <div className='flex flex-row items-center gap-1 text-[22px]'>
-                <Link href='/' className='cursor-pointer'>
-                  <Image src="/logo/digital-protein.svg" alt="company-logo" width={200} height={50} className="w-72 h-auto cursor-pointer" />
-                </Link>
-              </div>
-              <div className='text-[#77838F]'>
-                <div className='text-lg font-light mb-4'>
-                  Call us <br />
-                  <span className='text-[#1967d2] text-2xl font-bold'>+91 45454-34234</span>
-                </div>
-                <div className='text-[14px] font-light leading-6'>
-                  Address Here<br />
-                  Address Here<br />
-                  Address Here<br />
-                  info@digitalprotein.in <br />
-                </div>
-              </div>
-            </div>
-
+          <div className='min-w-64 flex flex-col gap-2 items-start justify-center w-auto text-[14px] text-[#77838F]'>
+            <p className='mb-2 text-[#fff] text-base font-semibold'>Services We Offer</p>
+            <Link href='/' className='font-normal md:w-64 w-full'>Mobile App Development</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>UI & UX Designs</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Software Development</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Management Systems</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>DevOps & Automation</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Blockchain Development</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Cloud Services</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Digital Transformation</Link>
           </div>
+
+          <div className='min-w-64 flex flex-col gap-2 items-start justify-center w-auto text-[14px] text-[#77838F]'>
+            <p className='mb-2 text-[#fff] text-base font-semibold'>Portfolio</p>
+            <Link href='/' className='font-normal md:w-64 w-full'>Alumni Management</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Institute Webpage</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>E-Commerce Webpage</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Learning Management System</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Portfolio Websites</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Job Portal</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>Event Management System</Link>
+            <Link href='/' className='font-normal md:w-64 w-full'>...more</Link>
+          </div>
+
+          <div className='min-w-64 flex flex-col gap-2 items-start justify-center w-auto text-[14px] text-[#77838F]'>
+            <p className='mb-2 text-[#fff] text-base font-semibold'>Resources</p>
+            <Link href='/company' className='font-normal md:w-64 w-full'>About Company</Link>
+            <Link href='/career' className='font-normal md:w-64 w-full'>Career With Us</Link>
+            <Link href='/contact' className='font-normal md:w-64 w-full'>Contact Us</Link>
+            <Link href='/insights' className='font-normal md:w-64 w-full'>Insights</Link>
+            <Link href='/faqs' className='font-normal md:w-64 w-full'>FAQS</Link>
+            <Link href='/terms-of-service' className='font-normal md:w-64 w-full'>Terms of Service</Link>
+            <Link href='/privacy-policy' className='font-normal md:w-64 w-full'>Privacy Policy</Link>
+          </div>
+
+          <div className='min-w-64 flex flex-col gap-3'>
+            <div className='flex flex-row items-center gap-1 text-[22px]'>
+              <Link href='/' className='cursor-pointer'>
+                <Image src="/logo/digital-protein.svg" alt="company-logo" width={200} height={50} className="w-72 h-auto cursor-pointer" />
+              </Link>
+            </div>
+            <div className='text-[#77838F]'>
+              <div className='text-lg font-light mb-4'>
+                Call us <br />
+                <span className='text-[#1967d2] text-2xl font-bold'>+91 45454-34234</span>
+              </div>
+              <div className='text-[14px] font-light leading-6'>
+                Address Here<br />
+                Address Here<br />
+                Address Here<br />
+                info@digitalprotein.in <br />
+              </div>
+            </div>
+          </div>
+
         </div>
 
         <hr className='bg-[#77838F]' />
 
+        <div className='w-full h-auto flex lg:flex-row flex-col md:justify-start justify-center py-4'>
+          <div className='flex flex-row items-center md:justify-start justify-center w-full text-sm text-[#77838F]'>
+            © 2025 Digital Protein. All Right Reserved.
+          </div>
+        </div>
+
       </div>
 
-      <div className='w-full h-auto flex lg:flex-row flex-col bg-[#151515] md:px-32 px-2 pb-8'>
-        <div className='flex flex-row items-center md:justify-start justify-center w-full text-sm text-[#77838F]'>
-          © 2025 Digital Protein. All Right Reserved.
-        </div>
-      </div>
     </div>
   )
 }
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -19,41 +19,36 @@ const Navbar = () => {
     };
 
     return (
-        <header className="fixed top-0 left-0 w-full py-4 flex justify-center items-center z-50 transition-all duration-300 border-b-2 border-[#175398] bg-[#00000080] backdrop-blur-[3px]">
+        <React.Fragment>
+            <header className="fixed top-0 left-0 w-full py-4 flex justify-center items-center z-50 transition-all duration-300 border-b-2 border-[#175398] bg-[#00000080] backdrop-blur-[3px]">
 
-            <div className='flex flex-row justify-between items-center w-10/12 h-full'>
-                <Link href={'/'}>
-                    <Image src="/logo/digital-protein.svg" alt="digital protein logo" width={200} height={50} className="w-52 h-auto cursor-pointer" />
-                </Link>
-
-                <div className="lg:hidden flex items-center text-white">
-                    <HamburgerMenuIcon onClick={() => setIsActive(!isActive)} className={isActive ? 'hidden' : 'flex w-5 h-5'} />
-                    <Cross1Icon onClick={() => setIsActive(!isActive)} className={isActive ? 'flex w-5 h-5' : 'hidden'} />
-                </div>
-
-                <nav className="text-white text-center lg:flex hidden lg:w-auto lg:space-x-8 justify-between items-center w-[55%]">
-                    <Link href={'/services'} className={`${currentTab === 'services' && 'text-[#175398]'} cursor-pointer hover:text-[#175398]`}>Services</Link>
-                    <Link href={'/company'} className={`${currentTab === 'company' && 'text-[#175398]'} cursor-pointer hover:text-[#175398]`}>Company</Link>
-                    <Link href={'/portfolio'} className={`${currentTab === 'portfolio' && 'text-[#175398]'} cursor-pointer hover:text-[#175398]`}>Portfolio</Link>
-                    <Link href={'/career'} className={`${currentTab === 'career' && 'text-[#175398]'} cursor-pointer hover:text-[#175398]`}>Career</Link>
-                </nav>
-
-                <Link href='/contact' className="hidden lg:block">
-                    <Button variant={currentTab === 'contact' ? 'manual' : 'default'} className="relative text-base font-normal">
-                        Schedule a Call
-                    </Button>
-                </Link>
-            </div>
-
-            <div className={`${isActive ? 'flex' : 'hidden'} fixed top-0 left-0 w-full h-[100vh] bg-black text-white transition-all flex-col items-center gap-4`}>
-
-                <div className='md:w-10/12 w-11/12 h-auto flex flex-row justify-between py-4 items-center gap-5 text-base shadow'>
-                    <Link href='/' className='cursor-pointer'>
-                        <Image src='/logo/digital-protein.svg' alt='logo digital protein' width={200} height={80} className='w-24' />
+                <div className='flex flex-row justify-between items-center w-10/12 h-full z-40'>
+                    <Link href={'/'}>
+                        <Image src="/logo/digital-protein.svg" alt="digital protein logo" width={200} height={50} className="w-52 h-auto cursor-pointer" />
                     </Link>
 
-                    <Cross1Icon className={`cursor-pointer ${isActive ? 'block' : ''}`} onClick={toggleNavbar} />
+                    <div className="md:hidden flex items-center text-white">
+                        <HamburgerMenuIcon onClick={() => setIsActive(!isActive)} className={isActive ? 'hidden' : 'flex w-5 h-5'} />
+                        <Cross1Icon onClick={() => setIsActive(!isActive)} className={isActive ? 'flex w-5 h-5' : 'hidden'} />
+                    </div>
+
+                    <nav className="text-white text-center lg:flex hidden lg:w-auto lg:space-x-8 justify-between items-center w-[55%]">
+                        <Link href={'/services'} className={`${currentTab === 'services' && 'text-[#175398]'} cursor-pointer hover:text-[#175398]`}>Services</Link>
+                        <Link href={'/company'} className={`${currentTab === 'company' && 'text-[#175398]'} cursor-pointer hover:text-[#175398]`}>Company</Link>
+                        <Link href={'/portfolio'} className={`${currentTab === 'portfolio' && 'text-[#175398]'} cursor-pointer hover:text-[#175398]`}>Portfolio</Link>
+                        <Link href={'/career'} className={`${currentTab === 'career' && 'text-[#175398]'} cursor-pointer hover:text-[#175398]`}>Career</Link>
+                    </nav>
+
+                    <Link href='/contact' className="hidden lg:block">
+                        <Button variant={currentTab === 'contact' ? 'manual' : 'default'} className="relative text-base font-normal">
+                            Schedule a Call
+                        </Button>
+                    </Link>
                 </div>
+
+            </header >
+
+            <div className={`${isActive ? 'flex' : 'hidden'} fixed top-0 left-0 w-full h-[100vh] bg-black text-white transition-all flex-col items-center gap-4 py-20 z-40`}>
 
                 <div className='flex flex-col gap-3 items-start w-10/12'>
                     <Button
@@ -106,7 +101,7 @@ const Navbar = () => {
                 </div>
 
             </div>
-        </header >
+        </React.Fragment>
     )
 }
 
