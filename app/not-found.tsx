a4d936c421c1b00d6f1f27905b3169443d19b08f
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-04-03 22:39:57 +0530

    update files

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,17 +2,20 @@
 
 import React from 'react';
 import Link from 'next/link';
+import Navbar from '@/components/common/Navbar';
 
 export default function NotFound() {
   return (
-    <div className='w-full h-screen bg-[#212121] flex flex-col gap-10 items-center justify-center'>
+    <div className='w-full h-screen bg-[#212121] text-white flex flex-col gap-10 items-center justify-center'>
+      <Navbar />
+
       <div className='flex flex-col items-center justify-center'>
         <h1>404 - Page Not Found</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
         <div>
           Go back to the <Link href="/" className='transition-all font-bold hover:underline'>
             homepage
-          </Link>
+          </Link>.
         </div>
       </div>
       <div className="loader"></div>
